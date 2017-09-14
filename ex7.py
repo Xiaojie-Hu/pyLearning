3c0357b016553acda8645e0c18682be385510cdf
gangzhao <gang.zhao.model@gmail.com>
2017-09-14 22:14:40 +0200

    add new file

--- ex7.py
+++ ex7.py
@@ -1,1 +1,12 @@
-import datetime
+name=input("Enter a string:")
+name_reversed=name[::-1]
+
+if name==name_reversed:
+    print ("A palindrome string")
+else:
+    print ("Not a palindrom string")
+print(name_reversed)
+a = [5, 10, 15, 20, 25, 30, 35, 40]
+print(a[:])
+print(a[:-1])
+print(a[1:-1])
